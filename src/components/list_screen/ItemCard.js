ecea97ade7a37704f2eeea8d705037b35a849800
frpansbu <frpan@cs.stonybrook.edu>
2019-11-14 20:06:43 -0500

    downloaded react materialize + items cards

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -6,7 +6,10 @@ class ItemCard extends React.Component {
         return (
             <div className="card z-depth-0 todo-list-link pink-lighten-3">
                 <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">{item.description}</span>
+                    <span className="card-title">{item.description}</span>    
+                    <span className = "card-assigned">Assigned To: {item.assigned_to}</span>
+                    <span className = "card-due">{item.due_date}</span>
+                    <sapn className = "card-status">Test</sapn>
                 </div>
             </div>
         );
